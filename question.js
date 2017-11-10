eaf6330d6d56f6e17ff93a7ee6882e90b349564b
PJ Frias <peejfrias@gmail.com>
2017-11-10 12:28:32 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,20 @@
 'use strict';
+
+class Question {
+
+  static All() {
+    return this._All
+  }
+
+  save(){
+    this.constructor._All.push(this)
+  }
+
+  constructor(question) {
+    this.content = question;
+    this.save();
+  }
+
+}
+
+Question._All = [];
